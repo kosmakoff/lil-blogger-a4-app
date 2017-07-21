ea3f7a235235f44f9249daf62e99d55673b5c72c
Oleg Kosmakov <kosmakoff@gmail.com>
2017-07-21 13:30:22 +0200

    Auto-collapse navbar on links click. Fixes #1

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare const $: any;
+// import * as $ from 'jquery';
+// import 'bootstrap';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
-
+export class NavComponent implements OnInit, AfterViewInit {
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    $('#navbarToggleArea .nav-link').on('click', function () {
+      $('#navbarToggleArea').collapse('hide');
+    });
+  }
 }
