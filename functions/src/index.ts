84e96fe4417cea3ec01cf8e523003f78141d8f8e
Oleg Kosmakov <kosmakoff@gmail.com>
2017-08-02 18:33:50 +0200

    Add server-side function to update articles counter

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,20 +5,21 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase);
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-    // Grab the text parameter.
-    console.log(`Request Query`, req.query);
-    const original = req.query.text;
+// Updates counter for Articles
+exports.updateArticlesCounter = functions.database.ref('/articles/{pushId}')
+    .onWrite(event => {
+        const isNew = event.data.exists() && !event.data.previous.exists();
+        const isDelete = !event.data.exists() && event.data.previous.exists();
 
-    if (original) {
-        // Push the new message into the Realtime Database using the Firebase Admin SDK.
-        admin.database().ref('/messages').push({ original: original }).then(snapshot => {
-            // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-            res.redirect(303, snapshot.ref);
+        const countRef = admin.database().ref('counters/articles');
+
+        return countRef.transaction(current => {
+            if (isNew) {
+                return (current || 0) + 1;
+            } else if (isDelete) {
+                return (current || 0) - 1;
+            } else {
+                return (current || 0);
+            }
         });
-    } else {
-        res.status(400).send('Wrong request query :(');
-    }
-});
+    });
