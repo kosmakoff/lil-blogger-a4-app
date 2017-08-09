d75bfacd459fe0d555b3a49f0b64ed91de3378cf
Oleg Kosmakov <kosmakoff@gmail.com>
2017-08-09 18:23:20 +0200

    Preview area for article body

--- src/app/shared/services/firebase.service.ts
+++ src/app/shared/services/firebase.service.ts
@@ -78,7 +78,6 @@ export class FirebaseService {
         return {
             displayName: userObject.displayName,
             email: userObject.email,
-            phoneNumber: userObject.phoneNumber,
             photoURL: userObject.photoURL,
             providerId: 'google.com',
             uid: userObject.uid
